f3de0de96345677a2367cef937d78f29597d394d
Charie Smith <charlie.smith1@sainsburys.co.uk>
2016-12-09 17:08:12 +0000

    Lesson3 progress

---
+++ lesson3/code/concurrency.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// START OMIT
+
+func main() {
+	pointlessFunc("Hello gophers")
+}
+
+func pointlessFunc(msg string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(msg, i)
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+	}
+}
+
+// END OMIT
---
+++ lesson3/code/concurreny2.go
@@ -0,0 +1,23 @@
+package main
+
+func test() {
+	// START DECLARE OMIT
+	// Declaring a channel type
+	var mychannel chan int
+
+	// Initializing a channel
+	mychannel = make(chan int)
+	newchannel := make(chan int)
+
+	// Sending on a channel
+	// The arrow inticates the direction of data
+	mychannel <- 1
+
+	// receiving on a channel
+	value = <-mychannel
+	// END DECLARE OMIT
+}
+
+func main() {
+
+}
