774f763a4264983c95a679b5dc4af13b7f97e051
Charie Smith <charlie.smith1@sainsburys.co.uk>
2017-01-10 11:10:49 +0000

    Fimish lesson4

---
+++ lesson4/code/nilinterface.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// START MAPS INTERFACES OMIT
+	var b interface{}
+	fmt.Println(b)
+	// END MAPS INTERFACES OMIT
+}
---
+++ lesson4/code/nilstuff.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// START MAPS OMIT
+	var a map[int]string
+	// END MAPS OMIT
+
+	// START MAPS MAKE OMIT
+	a = make(map[int]string)
+	// END MAPS MAKE OMIT
+
+}
